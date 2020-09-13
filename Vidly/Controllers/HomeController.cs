9513f39df15c8195f2a913c32c14d1e01177fab4
Peculiar <omobabson@gmail.com>
2020-09-13 02:03:17 +0100

    Add project files.

---
+++ Vidly/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        
+            public ActionResult Movie()
+        {
+            var movie = new Movies() { Name = "Shrek!" };
+
+
+            return View(movie);
+        }
+    }
+}
